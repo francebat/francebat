46e0aa6a4561d6b041a00b063821feea788912fb
France Bat <francebat@outlook.fr>
2016-12-30 13:26:58 +0100

    [AYDOGAN Mehmet] Création du projet

---
+++ francebat/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(francebat.Startup))]
+namespace francebat
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ francebat/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="francebat.MvcApplication" Language="C#" %>
